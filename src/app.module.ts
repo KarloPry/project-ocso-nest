7db0087a8e2e25b780877897a5d84e4f138ddf41
karlo-pry <karlopazpory@gmail.com>
2024-08-04 18:42:09 -0600

    Created three new resources to expand backend capabilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { ProductsModule } from "./products/products.module";
 import { ConfigModule } from "@nestjs/config";
 import { Product } from "./products/entities/product.entity";
 import { ProvidersModule } from './providers/providers.module';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 
 @Module({
   imports: [
@@ -25,6 +28,9 @@ import { ProvidersModule } from './providers/providers.module';
     EmployeesModule,
     ProductsModule,
     ProvidersModule,
+    ManagersModule,
+    LocationsModule,
+    RegionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
