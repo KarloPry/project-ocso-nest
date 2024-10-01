9d1443927c6c89defb15d0629beadf99e87242aa
karlo-pry <karlopazpory@gmail.com>
2024-10-01 16:21:11 -0600

    feat: changes to cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -50,7 +50,7 @@ export class AuthController {
     response.cookie(TOKEN_NAME, token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "lax",
       domain: process.env.cookiesDomain,
       expires: expireDate,
       maxAge: 1000 * 60 * 60 * 24 * 7,
